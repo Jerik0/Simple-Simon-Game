9916e452c757eba95dc40aff22cd0449ee3926c1
jerik0 <jcarrico1985@live.com>
2017-05-04 12:35:49 -0500

    COMPLETELY finished. Updated with game-over banner and reset functionality.

--- logic.js
+++ logic.js
@@ -12,7 +12,10 @@ $(document).ready(function() {
     var input = $('.box');
     var playButton = $('#play');
     var stopButton = $('#stop');
-    var audio = new Audio('audio/Spellbound.mp3');
+    var audio = new Audio('audio/Master Disorder.mp3');
+    var repeatSound = new Audio('audio/deep.wav');
+    var gameOverCounter = $('#game-over-counter');
+    var seconds = 10;
     var i=0;
     var roundCounter = $('#round-count');
     var roundCountText = $('#round-count-text');
@@ -20,41 +23,52 @@ $(document).ready(function() {
     var generatedSequence = [];
 
     //----------------------------------------------------------------------------------------------------------------
-    //TODO: Change how the generator is activated (as opposed to being activated by start button).
-    //TODO: Iterator will not animate second consecutive animation on same element
-    //TODO: Complete 'game over' functionality: 'restart' button appears and resets everything.
-    //TODO: Change start button when clicked on.
-    //TODO: Add difficulty functionality and slider - easy, medium and hard
-    //TODO: Add sounds and (maybe) music.
-    //TODO: Work on 'enter' key functionality.
 
-    $('#nav-bar-content').hover(function() {
-        $('#nav-bar-content').animate({
-            right: "0px"
-        })
-    });
+    audio.volume = .35;
 
-    $('#nav-bar-content').mouseleave(function() {
-        $('#nav-bar-content').animate({
-            right: "-175px"
-        })
-    });
+    function countDown() {
+        var secondCounter = setInterval(function() {
+            tryAgainClick(secondCounter);
+            gameOverCounter.html(seconds--);
+            if (seconds == 0) {
+                clearInterval(secondCounter);
+            }
+        }, 1000);
+    }
 
     //calls the specified functions when the "Start Game" button is clicked.
-    startGame.click(function() {
-        roundCounter.html('1');
+    function startUp() {
+        startGame.click(function () {
+            roundCounter.html('1');
+            roundCountText.html('Round Count: ')
+            input.removeClass('wrong');
+            up.removeClass('broken-up');
+            down.removeClass('broken-down');
+            left.removeClass('broken-left');
+            right.removeClass('broken-right');
+            $('#game-over').animate({
+                top: "-70px"
+            });
 
-        startGame.html('Now Playing');
+            //Begins generating randomly.
+            randomGenerate();
 
-        //Begins generating randomly.
-        randomGenerate();
+        });
+    }
+    startUp();
 
-    });
+    function tryAgainClick(secondCounter) {
+        tryAgain.click(function () {
+            clearTimeout(secondCounter);
+            startGame.show();
 
-    tryAgain.click(function() {
-        tryAgain.attr('id', 'start-game');
-        startGame.html('Now Playing');
-    });
+            $('#quarter-slot').show();
+            startGame.html('Start Game');
+            $('#game-over').animate({
+                top: "-70px"
+            });
+        });
+    }
 
     playButton.click(function() {
         audio.play();
@@ -64,16 +78,7 @@ $(document).ready(function() {
         audio.pause();
     });
 
-    function enter() {
-        if (event.key === "Enter") {
-            // console.log('Enter key worked');
-
-            //Begins generating randomly.
-            randomGenerate();
-        }
-    }
-
-    //Function that randomly generates a sequence from array 'options' and put the results into the array named 'generatedSequence'.
+    //Randomly generates a sequence from array 'options' and put the results into the array named 'generatedSequence'.
     function randomGenerate () {
         //start the count at 0 again
         i=0;
@@ -85,50 +90,48 @@ $(document).ready(function() {
 
     //----------------------------------------------------------------------------------------------------------------
 
-        //This just adds the ability for the browser to listen for key presses and sets the behavior for them.
-        window.addEventListener("keydown", function (event) {
+    //Adds the ability for the browser to listen for key presses and sets the behavior for them.
+    window.addEventListener("keydown", function (event) {
 
-            if(event.key === "Enter"){
-                startGame.click();
+        if(event.key === "Enter"){
+            startGame.click();
+        }
+
+        switch (event.key) {
+            case "ArrowUp": {
+                up.removeClass('blink');
+                setTimeout(function () {
+                    up.addClass('blink');
+                }, 1);
             }
-
-            switch (event.key) {
-                case "ArrowUp": {
-                    up.removeClass('blink');
-                    setTimeout(function () {
-                        up.addClass('blink');
-                    }, 1);
-                }
-                    break;
-                case "ArrowDown": {
-                    down.removeClass('blink');
-                    setTimeout(function () {
-                        down.addClass('blink');
-                    }, 1);
-                }
-                    break;
-                case "ArrowLeft": {
-                    left.removeClass('blink');
-                    setTimeout(function () {
-                        left.addClass('blink');
-                    }, 1);
-                }
-                    break;
-                case "ArrowRight": {
-                    right.removeClass('blink');
-                    setTimeout(function () {
-                        right.addClass('blink');
-                    }, 1);
-                }
-                    break;
+                break;
+            case "ArrowDown": {
+                down.removeClass('blink');
+                setTimeout(function () {
+                    down.addClass('blink');
+                }, 1);
             }
+                break;
+            case "ArrowLeft": {
+                left.removeClass('blink');
+                setTimeout(function () {
+                    left.addClass('blink');
+                }, 1);
+            }
+                break;
+            case "ArrowRight": {
+                right.removeClass('blink');
+                setTimeout(function () {
+                    right.addClass('blink');
+                }, 1);
+            }
+                break;
+        }
 
-            enter();
+        match();
+    });
 
-            match();
-        });
-
-    //----------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------
 
     //Create a function to read user's input and match it to array "generatedSequence".
     function match() {
@@ -148,20 +151,49 @@ $(document).ready(function() {
             } else {
                 console.log("Round not complete yet");
             }
+        } else if (event.key === 'Enter') {
+
         } else {
+            console.log('game over');
             gameOver();
         }
     }
 
     //Adds functionality to animate gameboard initially.
     function gameOver() {
+        seconds = 10;
+
+        console.log('game over');
+
+        //adds a class to the current sequence index and the gamebox that flashes red.
         $('#' + generatedSequence[i]).addClass('wrong');
         gameBox.addClass('wrong');
+
+        //sets the generatedSequence to empty.
         generatedSequence = [];
-        startGame.attr('id', 'try-again');
-        startGame.html('TRY AGAIN');
+
+        //makes the start-game button and its corresponding quarter-slot disappear.
+        startGame.hide();
+        $('#quarter-slot').hide();
+
+        //hides the start button after it disappears.
+
+        //sets the round count back to nothing.
+
         roundCounter.html('');
+
+        //makes the text of "round count" change to "GAME OVER".
         roundCountText.html('GAME OVER');
+
+        //after 1 second, the game over banner will drop down to 10px from the top of the screen.
+        setTimeout(function() {
+            $('#game-over').animate({
+                top: "10px"
+            });
+        }, 1000);
+
+        countDown();
+
         setTimeout(function() {
             breakGame();
         }, 1000);
@@ -169,6 +201,7 @@ $(document).ready(function() {
 
     //Function that animates the game Board when game Over.
     function breakGame() {
+        startGame.addClass('broken-start');
         up.addClass('broken-up');
         down.addClass('broken-down');
         left.addClass('broken-left');
@@ -177,12 +210,16 @@ $(document).ready(function() {
 
     //This function repeats back the sequence that has been randomly generated and put inside the array 'generatedSequence'.
     function iterator() {
-
         //sets its counter to zero inside the function
         var count = 0;
 
-                         //sets Interval at which to execute iterator
+        //sets Interval at which to execute iterator
         var intervalId = setInterval(function() {
+
+            //plays the sound of the computer repeating back.
+            if(count < generatedSequence.length) {
+                repeatSound.play()
+            }
 
             // if the variable 'count' is equal to the length of the array 'generatedSequence' (once it has reached the end of the array)
             if (count === generatedSequence.length) {
@@ -195,7 +232,6 @@ $(document).ready(function() {
             $('.box').removeClass('blink');
 
             //adds the class 'repeat' to the array "generatedSequence"'s current index which is at position: 'count'.
-            // $('#' + generatedSequence[count]).addClass('repeat');
             $('#' + generatedSequence[count]).addClass('repeat');
 
             //sets a delay on removing the class 'repeat' so it will animate before removing.
@@ -207,9 +243,9 @@ $(document).ready(function() {
         }, 800);
     }
 
-    //a function whos parameter's first letter will be replaced (with slice) and capitalized.
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
+    //a function whose parameter's first letter will be replaced (with slice) and capitalized.
+    function capitalizeFirstLetter(String) {
+        return String.charAt(0).toUpperCase() + String.slice(1);
     }
 
 //----------------------------------------------------------------------------------------------------------------
